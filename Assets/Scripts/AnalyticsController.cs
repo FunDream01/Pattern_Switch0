c88d85c0774304ece1c6a7c6be10367972a42cd1
Abdulrahman Adham <46246621+FunDream01@users.noreply.github.com>
2020-02-20 21:28:33 +0300

    GameAnalytics Added

---
+++ Assets/Scripts/AnalyticsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameAnalyticsSDK;
+public class AnalyticsController : MonoBehaviour
+{
+    public static AnalyticsController instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameAnalytics.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LogLevelStarted(int level)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, level.ToString());
+    }
+
+    public void LogLevelFailed(int level)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Fail, level.ToString());
+    }
+
+    public void LogLevelSucceeded(int level)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, level.ToString());
+    }
+}
